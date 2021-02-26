3d7a36fa2d24c2c7dfda46dfe5facaef02671b10
Narit Charoenval <naritsociety@gmail.com>
2021-02-26 14:56:25 +0700

    Add project files.

---
+++ dotNet_MVC01/Models/BeerModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNet_MVC01.Models
+{
+    public class BeerModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+    }
+
+    public class Beer
+    {
+        public List<BeerModel> GetAllBeer()
+        {
+            return new List<BeerModel>
+            {
+                new BeerModel { Id =1, Name="Redhorse", Type="Lager" },
+                new BeerModel { Id =2, Name="Furious", Type="IPA" },
+                new BeerModel { Id =3, Name="Guinness", Type="Stout" },
+                new BeerModel { Id =4, Name="Sierra", Type="Ale" },
+                new BeerModel { Id =5, Name="Stella", Type="Pilsner" },
+            };
+        }
+    }
+}
